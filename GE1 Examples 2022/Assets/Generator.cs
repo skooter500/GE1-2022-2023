816e58021c3a9e78d1ff4f7edf27a7b1290e2b3a
Squange <78355315+SquangeEj@users.noreply.github.com>
2022-10-04 16:44:11 +0100

    Jazzed it up a bit

--- GE1 Examples 2022/Assets/Generator.cs
+++ GE1 Examples 2022/Assets/Generator.cs
@@ -6,14 +6,32 @@ public class Generator : MonoBehaviour
 {
     public int loops = 10;
     public GameObject prefab;
+    public float MaxRadius;
+
     // Start is called before the first frame update
     void Start()
     {
+
+
+        for(float Radius = 2; Radius < MaxRadius; Radius++)
+        {
+            for (float i = 0; i < 360; i += 30 /(Radius/2))
+            {
+                float angle = i  * Mathf.Deg2Rad;
+                Vector2 SpawnPos;
+                SpawnPos.x = (Radius * Mathf.Cos(angle)) +transform.position.x;
+                SpawnPos.y = (Radius * Mathf.Sin(angle)) +transform.position.y;
+                var Gobj = Instantiate(prefab, SpawnPos, transform.rotation);
+                Gobj.transform.parent = transform;
+            }
+        }
+       
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-
+        transform.Rotate(0,0,0.1f);
     }
+
+
 }
