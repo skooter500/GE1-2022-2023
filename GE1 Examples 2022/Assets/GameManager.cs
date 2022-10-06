3bb60a7ec21ba1c13cfdb4518a5dc2a665f932b3
Hussain992077 <hussaini992077@gmail.com>
2022-10-06 17:50:56 +0100

    lab2solution

---
+++ GE1 Examples 2022/Assets/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Instance;
+
+    public GameManager()
+    {
+        Instance = this;
+    }
+
+    private static StringBuilder message = new StringBuilder();
+
+    public void OnGUI()
+    {
+        GUI.color = Color.white;
+        GUI.Label(new Rect(0, 0, Screen.width, Screen.height), "" + message);
+        if (Event.current.type == EventType.Repaint)
+        {
+            message.Length = 0;
+        }
+    }
+
+    public static void Log(string text)
+    {
+        message.Append(text + "\n");
+    }
+}
