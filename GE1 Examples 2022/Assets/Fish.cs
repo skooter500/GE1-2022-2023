ce1f4e24cedcf84055d4b27616ec24d09b940e58
ahmeda625 <aalazawy625@gmail.com>
2022-10-13 19:25:11 +0100

    mylab3

---
+++ GE1 Examples 2022/Assets/Fish.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    public Transform head;
+    public Transform tail;
+    [Range(0.0f, 5.0f)]
+    public float frequency = 0.5f;
+    public float headAmplitude = 40;
+    public float tailAmplitude = 60;
+
+    public float theta = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theta = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Your code goes here!
+
+    }
+}
