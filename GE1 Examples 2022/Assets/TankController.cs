966de2e4f038021b2ccdbc7d817b55d8a028fca5
ahmeda625 <aalazawy625@gmail.com>
2022-10-20 22:21:53 +0100

    myLab4

---
+++ GE1 Examples 2022/Assets/TankController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+    public float speed = 5;
+    public float rotSpeed = 100;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float c = Input.GetAxis("Vertical");
+        transform.Translate(0, 0, c * speed * Time.deltaTime);
+
+        float r = Input.GetAxis("Horizontal");
+        transform.Rotate(0, r * rotSpeed * Time.deltaTime, 0);
+    }
+}
